7a72f53a933885ee0bcf357da2d90d92a7a0f97a
theoCoding87 <tomge@hotmail.de>
2023-11-10 22:25:52 +0100

    works as intended to do.

--- skript.py
+++ skript.py
@@ -4,6 +4,7 @@
 
 from fpdf import FPDF 
 from tabulate import tabulate
+from datetime import datetime
 import csv
 import random
 
@@ -36,55 +37,21 @@ def testObjectValues(path):
             print("#### EndOfCheck ####\n")
 
 
-
-
-### neuer Ansatz:
-### Class 
-###     constructor (?) with given path
-###     method to print to Table
-###     method: assign Values Vorname, Nachname, Personalnummer, Passwort
-###         display the vales to user as a printed table in command line
-###         user-input from console 1,2,3,4
-###     method: create PDF
 class ConsolePdfBuilder:
     def __init__(self, path):
         # list which holds listElement for each given Value for an Employee in source.txt, will be the header for tabulate
-        self.mlistHead = [] 
+        self.mListHead = [] 
         # dict to hold demanded Values and its assigmets, initiated with the values 0
         self.mDictValuePairs = {"strFirstName":0, "strLastName":0, "intUsername":0}
-
+        self.listTableData = []
 
     def PrintWelcomeScreen(self):
         print("\n   ||-------------------------------------------------------------||")
         print("   ||        PDF-Gestalter für Momox-Einstellungen Leipzig        ||")
-        print("   || Quelle des Datensatzes ist source.txt                       ||")
-        print("   || aktuelle Reihenfolge: Vorname, Nachname, lej_1234, Passwort ||")
+        print("   ||            Quelle des Datensatzes ist source.txt            ||")
+        print("   ||    Ausgegeben wird der Datensatz mit einem Tabellenkopf.    ||")
+        print("   ||    Verwertbare Trennzeichen sind Tabulatur und Eingabe.     ||")
         print("   ||-------------------------------------------------------------||\n")
-
-    def EvaluatePath(self,path):
-        if type(path) != str:
-            print("Function Error: Function expects Datatype of String.")
-        #Todo: Exception for wrong string of path
-        #except OSError as e:
-            #print("Functin Error: No such File.")
-        else:
-            with open(path, "r") as f:
-                listEmployee = [line.rstrip("\n") for line in f]
-                print("\n#### Check Object and its Values #### (You can expect 6 Lines)")
-                print(" >   Object Type     : " + str(type(listEmployee)))
-                count = 0
-                for i in listEmployee:
-                    count += 1
-                print(" >   Number of items :", count)
-                ValueFirstItem= listEmployee[0].split("\t")     #Todo: changed firrst value to index of 0 -> still working?
-                arrWrittenNumber = ["1st","2nd","3rd","4th"]
-                countNum = 0
-                for i in ValueFirstItem:
-                    print(" >  ", arrWrittenNumber[countNum], "value is ", ValueFirstItem[countNum])
-                    countNum += 1
-                #print("First Item is ", ValueFirstItem[0], " | ", ValueFirstItem[1])
-                print("#### EndOfCheck ####\n")
-
 
     # Functions set Dictionary indicees for the needed values: First Name, Last Name, PersonalID
     def SetFirstNameIndex(self, i):
@@ -96,7 +63,7 @@ class ConsolePdfBuilder:
         
     ### first of two functions to create a password
     ### function to create random constellation of a font-independent distinguishable tripplet made of items out of four predefined lists
-    def createStringTriplet():
+    def createStringTriplet(self):
         listConsonant = [] 
         # first list of the list / array -> fine-print consonants
         listConsonant.append(["b", "c", "d", "f", "g", "h", "k", "m", "n", "p", "q", "r", "u", "v", "w", "x", "y", "z"])
@@ -123,7 +90,7 @@ class ConsolePdfBuilder:
     ### second function to create a password
     ### function to create random constellation of a duplet from a predefined list
     ### it is possible that the duplet has two numbers and no special character, or the other way around
-    def createSpecialCharacterDuplet():
+    def createSpecialCharacterDuplet(self):
         listSymbolAndNumber = []
         # no 7 due to its similar appearance to t and so on. no tilde, no accents, no square brackets.
         listSymbolAndNumber.append(["!", "$", "%", "&", "/", "(", ")", "?", "#", ":", ";", "=", "2","3","4","5","6","8","9"])
@@ -137,72 +104,82 @@ class ConsolePdfBuilder:
 
     ### main function to create the actual password
     ### characteristics: holds 8 digits, two of them are a combination of special characters and / or numbers
-    def createPassword():
-        strPassword = str(createStringTriplet()) + str(createSpecialCharacterDuplet()) + str(createStringTriplet())
+    def createPassword(self):
+        strPassword = str(self.createStringTriplet()) + str(self.createSpecialCharacterDuplet()) + str(self.createStringTriplet())
         return strPassword
 
-    # Function to concatenate the Personal Number and Prefix lej_
-    def createUserName(self, MomoxPersNr):
-        userName = "lej_" + str(MomoxPersNr)
-        return userName
 
-    def PrintTable(self,path):
+    def PrintTable(self,listTableData):
         ### Prints out Table on Console. Table head is filled with numbers as indicees.
         ### User has to input Numbers for relevant Values:
         ### Vorname, Nachname, Personalnummer
         
         with open("source.txt","r") as f:
             listEmployee = [line.rstrip("\n") for line in f]
-            # Detect count of attributes
-            numOfAttributes = 0
+            
+            
+            # get mListHead as List of Indicees for Table Head
+            numOfAttributes = 0 # as first Index of Table Head
             listFirstEmployee = listEmployee[0].split("\t")
             for i in listFirstEmployee:
-                    self.mlistHead.append(numOfAttributes)
+                    self.mListHead.append(numOfAttributes)
                     numOfAttributes += 1
                             
+            
             ### create object containing given employees for body of table:
-            listTableData = []
-            count = 0
-
-            #for i in listEmployee:
-            currentEmployee = listEmployee[count].split("\t")
-            for b in range(len(currentEmployee)):
-                listTableData.append(listEmployee[count].split("\t"))
-                count += 1
-            count = 0
-            print(tabulate(listTableData, headers=self.mlistHead, tablefmt="grid"))
+            index = 0
+            currentEmployee = listEmployee[index].split("\t")
+            for b in range(len(listEmployee)):
+                self.listTableData.append(listEmployee[index].split("\t"))
+                index += 1
+            print(tabulate(self.listTableData, headers=self.mListHead, tablefmt="grid"))
         return 1
 
-    def buildPdf(self):
+    
+    def buildPdf(self, listTableData, mDictValuePairs):
         pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("helvetica", "B", 16)
-        pdf.cell(40, 10, "Momox")
-        #pdf.image('logo.jpg', x = None, y = None, type = 'jpg',)
-        pdf.output("/export/output.pdf")
+        a = 0
+        for i in listTableData:
+            pdf.add_page()
+            pdf.set_left_margin(25)
+            pdf.set_font("Helvetica", style = "", size = 22)
+            # create a dummy cell to get the position for the second cell under the momox-logo
+            pdf.cell(w=10, h=44, ln=1)
+            pdf.set_font("Helvetica", style="", size=22)
+            # for creating the visual design, it is very usefull to make the borders visible.
+            # pdf.cell(w=180, h=18, txt="Vorname", border=1, ln=1)
+            pdf.cell(w=180, h=16, txt=listTableData[a][int(x.mDictValuePairs["strFirstName"])], ln=1)
+            pdf.cell(w=180, h=16, txt=listTableData[a][int(x.mDictValuePairs["strLastName"])], ln=1)
+            # create a dummy cell to get the position for the next cell 
+            pdf.cell(w=10, h=44, ln=1)
+            pdf.set_font("Helvetica", style="", size=28)
+            pdf.cell(w=55, h=18, txt="Username: ")
+            pdf.set_font("Helvetica", style="B", size=28)
+            pdf.cell(w=60, h=18, txt="lej_" + listTableData[a][int(x.mDictValuePairs["intUsername"])],ln=1)   
+            pdf.set_font("Helvetica", style="", size=28)
+            pdf.cell(w=55, h=18, txt="Passwort: ")
+            pdf.set_font("Helvetica", style="B", size=28)
+            pdf.cell(w=60, h=18, txt=x.createPassword())
+            pdf.image('momox_logo.png', 130, 15,48)
+            a += 1
+        # Todo: parse Datetime into Filename
+        pdf.output("/export/einstellungen_" + datetime.today().strftime('%Y-%m-%d') + "output.pdf")
         return 1
-
-
-
-
 
 
 
 
 source = "source.txt"
 x = ConsolePdfBuilder(source)
-#x.PrintWelcomeScreen()
-#x.EvaluatePath(source)
-#x.PrintTable(source)
-#i = input("Welche Spalte ist Vorname: ")
-#x.SetFirstNameIndex(i)
-
-#i = input("Welche Spalte ist Nachname: ")
-#x.SetLastNameIndex(i)
-#i = input("Welche Spalte ist Personalnummer: ")
-#x.setPersonalId(i)
-#print(x.mDictValuePairs)
-x.buildPdf()
+x.PrintWelcomeScreen()
+# Display the given Data in a human-readable Table
+x.PrintTable(x.listTableData)
+# Getting Assignment-Indicees for the relevant Data
+x.SetFirstNameIndex(input("\nIn welcher Spalte steht der Vorname? "))
+x.SetLastNameIndex(input("In welcher Spalte steht der Nachname? "))
+x.setPersonalId(input("in welcher Spalte steht die Personalnummer? "))
+print("\n-> Das PDF wird erstellt ... siehe Verzeichnis [Export]")
+print("-> Danach werden die Container und Images gelöscht ...\n")
 
 
-print("---------------> convertion done.")
+x.buildPdf(x.listTableData, x.mDictValuePairs)
